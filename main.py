756b05a5b5abb2f33817e58c665d3d934394e84e
mrcdsc <78731092+mrcdsc@users.noreply.github.com>
2024-12-15 17:49:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 import mplfinance as mpf
 import datetime
 from datetime import datetime
-import scipy.stats as si
+import scipy.stats as si, stats
 
 ## Importing your Config ##
 
